8bfd52b43e53a90c2aea1b7375b87d767f2aa7cb
AlbertXe <563000409@qq.com>
2021-08-24 23:06:30 +0800

    init

---
+++ src/A.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: AlbertXe
+ * @create: 2021-08-24 22:38
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
